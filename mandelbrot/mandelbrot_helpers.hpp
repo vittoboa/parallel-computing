f81080a16c5b303f8149ecabc3876052063890bd
vittoboa <boarinivittorio@gmail.com>
2022-12-22 10:30:26 +0100

    Add parallel computation of mandelbrot using static work allocation

--- mandelbrot/mandelbrot_helpers.hpp
+++ mandelbrot/mandelbrot_helpers.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <complex>
 
 
 struct Image {
@@ -19,3 +20,21 @@ struct Image {
         return data[(((channel * height) + j) * width) + i];
     }
 };
+
+
+int mandelbrot_pixel_color(std::complex<double> c, int color)
+{
+    std::complex<double> z(0, 0);
+
+    int max_iterations = 2048;
+    for (int i = 0; i < max_iterations; i++) {
+        if (abs(z) > 4) {
+            color = 255;
+            break;
+        }
+
+        z = z * z + c;
+    }
+
+    return color;
+}
--- mandelbrot/mandelbrot_parallel.cpp
+++ mandelbrot/mandelbrot_parallel.cpp
@@ -1,12 +1,58 @@
+#include <thread>
+
 #include "mandelbrot_helpers.hpp"
+
+
+void worker_static(Image &image, int thread_id, int num_threads)
+{
+    // find the first and last row of the part of matrix which was statically assigned to the thread
+    int start = (image.height *  thread_id      / num_threads);
+    int end   = (image.height * (thread_id + 1) / num_threads);
+
+    // process the part of matrix assigned to the thread
+    for (int j = start; j < end; j++) {
+        for (int i = 0; i < image.width; i++) {
+            // find single number which represent two dimensional matrix position
+            double dx = ((double) i / image.width - 0.7) * 2;
+            double dy = ((double) j / image.height - 0.5) * 2;
+            std::complex<double> c = std::complex<double>(dx, dy);
+
+            // set gray-scale color based on thread-id
+            int color = (255 * thread_id) / num_threads;
+
+            // apply mandelbrot algorithm to find color which corresponds to the current pixel
+            int pixel_color = mandelbrot_pixel_color(c, color);
+
+            // apply to the image
+            for (int ch = 0; ch < image.channels; ch++) {
+                image(ch, j, i) = pixel_color;
+            }
+        }
+    }
+}
 
 
 int main(int argc, char **argv)
 {
+    // init variables with default values
+    int num_threads = std::thread::hardware_concurrency();
+    std::string work_allocation = "static";
+
     // define the image to plot the mandelbrot set
     int channels = 3; // red, green, blue
     int width = 800, height = 600;  // define size of output image
     Image image(channels, height, width);
+
+    // spawn threads and pass parameters
+	std::vector<std::thread> threads;  // vector to hold the thread objects
+    for (int t = 0; t < num_threads; t++) {
+        threads.push_back(std::thread(worker_static, std::ref(image), t, num_threads));
+    }
+
+    // join threads
+    for (auto& t : threads) {
+        t.join();
+    }
 
     return 0;
 }
