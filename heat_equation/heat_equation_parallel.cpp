df520cfa1aaeffca0e0c74ef10c0972d6fdeac41
vittoboa <boarinivittorio@gmail.com>
2022-12-29 23:18:02 +0100

    Initialize the two matrices used to parallelly compute the 2d heat equation

--- heat_equation/heat_equation_parallel.cpp
+++ heat_equation/heat_equation_parallel.cpp
@@ -1,7 +1,14 @@
 #include <mpi.h>
 
+#include "heat_equation_parallel.hpp"
+
+
 int main(int argc, char **argv)
 {
+    // default values
+    int cols_tot = 16;
+    int rows_tot = 16;
+
     // init MPI
     MPI_Init(&argc, &argv);
 
@@ -10,6 +17,27 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     // get current rank of current process
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // determine the number of rows of the process
+    int rows_proc = 2 + (rows_tot / nprocs) + ((rows_tot % nprocs > rank) ? 1 : 0);
+
+    // initialize the two matrices
+    Mat U(rows_proc, cols_tot);
+    Mat U_new(rows_proc, cols_tot);
+
+    // initilize the internal rows, overlapping regions and the boundaries of the matrices
+    for (int i = 0; i < rows_proc; i++) {
+        for (int j = 0; j < cols_tot; j++) {
+            U(i, j) = 0.0;
+        }
+    }
+
+    // initialize the last internal row of the last process to 100
+    if (rank == nprocs - 1) {
+        for (int j = 0; j < cols_tot; j++) {
+            U(rows_proc - 2, j) = 100.0;
+        }
+    }
 
     // end MPI
     MPI_Finalize();
---
+++ heat_equation/heat_equation_parallel.hpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+struct Mat {
+    int height;
+    int width;
+    std::vector<double> data;
+
+    Mat(int height, int width):
+        height(height),
+        width(width),
+        data(height*width)
+    {}
+
+    // get position in data vector
+    double& operator()(int j, int i) {
+        return data[(j * width) + i];
+    }
+};
